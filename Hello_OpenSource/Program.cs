14e1f2ac23653545d91943d03327d4dd0727156d
anusara57 <anusara.makk57@sskru.ac.th>
2016-09-09 14:27:47 +0700

    new Project

---
+++ Hello_OpenSource/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello_OpenSource
+{
+    class Hello
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello Anusara Makkasan 5711423118!");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
